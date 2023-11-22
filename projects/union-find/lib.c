2b43519e60193137812524c22700ec3f5855c525
Joe Barnett <joebarnett876@gmail.com>
2023-11-22 08:18:28 +0000

    union-find

--- projects/union-find/lib.c
+++ projects/union-find/lib.c
@@ -1,3 +1,27 @@
+#include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 
-void make_forest(size_t len) {}
+size_t find_root(size_t index, size_t forest[]) {
+  size_t parent = forest[index];
+  return parent == SIZE_MAX ? index : find_root(parent, forest);
+}
+
+void find_replace(size_t index, size_t new_parent, size_t forest[]) {
+  size_t parent = forest[index];
+  forest[index] = new_parent == index ? SIZE_MAX : new_parent;
+  if (parent != SIZE_MAX) {
+    find_replace(parent, new_parent, forest);
+  }
+}
+
+size_t find_compress(size_t index, size_t forest[]) {
+  size_t root = find_root(index, forest);
+  find_replace(index, root, forest);
+  return root;
+}
+
+void union_nodes(size_t index_a, size_t index_b, size_t forest[]) {
+  size_t root_a = find_compress(index_a, forest);
+  find_replace(index_b, root_a, forest);
+}
--- projects/union-find/lib.h
+++ projects/union-find/lib.h
@@ -1,1 +1,9 @@
-void foo(void) {}
+#include <stddef.h>
+
+size_t find_root(size_t index, size_t forest[]);
+
+void find_replace(size_t index, size_t new_parent, size_t forest[]);
+
+size_t find_compress(size_t index, size_t forest[]);
+
+void union_nodes(size_t index_a, size_t index_b, size_t forest[]);
