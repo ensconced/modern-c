80c821dcfbac41e05b9ace6020a2b4b8e0eeb007
Joe Barnett <joe.barnett@elliptic.co>
2023-11-29 12:23:34 +0000

    matmul

--- projects/matrix-math/test.c
+++ projects/matrix-math/test.c
@@ -1,5 +1,16 @@
+#include "./lib.h"
 #include <assert.h>
 
-void test_foo(void) {}
+void test_multiply(void) {
+  float a[] = {1, 2, 3, 4, 5, 6};
+  float b[] = {7, 8, 9, 10, 11, 12};
+  float expected_result[] = {58, 64, 139, 154};
+  float result[4] = {0};
+  multiply(a, b, 2, 3, 2, result);
 
-int main(void) { test_foo(); }
+  for (size_t i = 0; i < 4; ++i) {
+    assert(result[i] == expected_result[i]);
+  }
+}
+
+int main(void) { test_multiply(); }
